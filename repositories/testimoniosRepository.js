194f66ec3644f91d6de5cc5ce179685ee510c206
beto <56642267+be-tho@users.noreply.github.com>
2021-09-30 22:26:06 -0300

    put testimonios api
    Agregamos la posibilidad de a travez de la api por medio del verbo PUT cambiar el estado de deleted para que asi el front lo vea o no.

--- repositories/testimoniosRepository.js
+++ repositories/testimoniosRepository.js
@@ -29,5 +29,5 @@ export async function create(entity){
 }
 export default {
     getAll,
-    create
+    create,
 }
--- routers/testimoniosRouter.js
+++ routers/testimoniosRouter.js
@@ -18,14 +18,14 @@ router.route('/testimonios')
         fs.promises.readFile ('./data/testimonios.json')
             .then (function (data) {
                 const testimonios = JSON.parse (data)
-                const instrumento = req.body
-                instrumento.id = testimonios.length + 1
+                const testimonio = req.body
+                testimonio.id = testimonios.length + 1
 
-                testimonios.push (instrumento)
+                testimonios.push (testimonio)
 
                 fs.promises.writeFile ('./data/testimonios.json', JSON.stringify (testimonios))
                     .then (function () {
-                        res.status (200).json (instrumento)
+                        res.status (200).json (testimonio)
                     })
                     .catch (function (err) {
                         res.status (500).json ({err: 500, msg: "error al escribir el dato"})
@@ -60,9 +60,9 @@ router.route('/testimonios/:id')
             .then(function (data){
                 const testimonios = JSON.parse(data)
 
-                const instrumento = testimonios.find(e => e.id == id)
-                if (instrumento != undefined && instrumento.deleted != true ) {
-                    let index = testimonios.indexOf(instrumento)
+                const testimonio = testimonios.find(e => e.id == id)
+                if (testimonio != undefined && testimonio.deleted != true ) {
+                    let index = testimonios.indexOf(testimonio)
                     testimonios[index] = {...req.body, id:id}
 
                     fs.promises.writeFile ('./data/testimonios.json', JSON.stringify (testimonios))
@@ -126,5 +126,33 @@ router.route('/testimonios/:id')
                 res.status(500).json({err: 500, msg: "Error al leer los datos"})
             })
     })
+
+
+router.route('/deleted/:id')
+    .put(function (req, res){
+        const id = parseInt(req.params.id)
+        fs.promises.readFile('./data/testimonios.json')
+            .then(function (data){
+                const testimonios = JSON.parse(data)
+                const testimonio = testimonios.find(e => e.id == id)
+                const name = testimonio.name
+                if (testimonio.deleted == true) {
+                    let index = testimonios.indexOf(testimonio)
+                    testimonios[index] = {...req.body, id:id, name:name}
+
+                    fs.promises.writeFile ('./data/testimonios.json', JSON.stringify (testimonios))
+                        .then (function () {
+                            res.status (200).json (testimonios[index])
+                        })
+                        .catch (function (err) {
+                            res.status (500).json ({err: 500, msg: "error al escribir el dato"})
+                        })
+                }
+            })
+            .catch(function (){
+                res.status(500).json({err: 500, msg: "Error al leer los datos"})
+            })
+    })
+    .post
 
 export default router
